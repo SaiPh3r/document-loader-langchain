a918add558b7312f9b1cc21da1fc0e26f56cde62
SaiPh3r <saianand750@gmail.com>
2025-10-01 23:18:40 +0530

    model fixed

--- text-loader.py
+++ text-loader.py
@@ -1,11 +1,25 @@
 from langchain_community.document_loaders import TextLoader
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+load_dotenv()
 
 loader = TextLoader('requirements.txt')
+model = ChatGoogleGenerativeAI(model="models/gemini-2.5-flash") 
+promt = PromptTemplate(
+    template='write a basic information about following requirement{req}' , 
+    input_variables=['req']
+)
+
+parser = StrOutputParser()
 
 doc = loader.load()
 
-print(type(doc))
+chain = promt | model | parser
 
-print(doc[0])
+print(doc[0].page_content)
 
-print(type(doc[0]))
+result = chain.invoke({'req':doc[0].page_content})
+print(result)
+
